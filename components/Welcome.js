0749266514b419cb72f717b2c68a092251fadce4
Kirill Sofronov <2138870+sokirill@users.noreply.github.com>
2022-11-30 16:18:01 +0800

    :mag: Add descriptions
    update welcome message and title tag

--- components/Welcome.js
+++ components/Welcome.js
@@ -1,8 +1,6 @@
 import siteMetadata from "@/data/siteMetadata";
 import Icon from "@/components/icons";
 import Image from "next/image";
-import { RoughNotation } from "react-rough-notation";
-import Link from "next/link";
 
 export default function Welcome() {
   return (
@@ -15,15 +13,14 @@ export default function Welcome() {
           width={250}
           className="rounded-full"
         />
-        <div className="flex space-x-3 pt-6 hover:text-primary-600 dark:hover:text-primary-400">
+        <div className="hover:text-primary-600 dark:hover:text-primary-400 flex space-x-3 pt-6">
           <Icon kind="twitter" href={siteMetadata.twitter} />
           <Icon kind="linkedin" href={siteMetadata.linkedin} />
           <Icon kind="github" href={siteMetadata.github} />
         </div>
       </div>
-      <div className=" prose pt-8 pb-8 dark:prose-dark xl:col-span-2">
+      <div className="prose dark:prose-dark pt-8 pb-8 xl:col-span-2">
         <h2>Hey, I am Kirill</h2>
-
         <p>
           I am a PM and indie-maker based in Singapore. Love to tinker with side
           projects whilst self-teaching software development and design.
